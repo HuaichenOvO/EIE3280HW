e1424abb8baf715a4e9372e2ca6b0bed1e62f3d6
HuaichenOvO <59914808+HuaichenOvO@users.noreply.github.com>
2022-04-14 16:45:20 +0800

    updated 12.1.py

--- 12.1.py
+++ 12.1.py
@@ -3,20 +3,18 @@ import numpy as np
 from scipy.optimize import minimize_scalar
 
 def f(p):
+
+    def ff(p):
+        return 8 * np.exp(-(p/4.0)) + 6 * np.exp(-(p/2.0))
+
     if (p < 0.96):
-        return (8 * np.exp(-(p/4.0)) + 6 * np.exp(-(p/2.0))-10) + p*(14 - 8 * np.exp(-(p/4.0)) - 6 * np.exp(-(p/2.0)))
+        return (ff(p)-10) + p*(14 - ff(p))
 
     elif (p>=0.96 and p<1.27):
-        return p*(14 - 8 * np.exp(-(p/4.0)) - 6 * np.exp(-(p/2.0)))
+        return p*(14 - ff(p))
 
     elif (p>=1.27): 
-        return (9 - 8 * np.exp(-(p/4.0)) - 6 * np.exp(-(p/2.0))) + p*(14 - 8 * np.exp(-(p/4.0)) - 6 * np.exp(-(p/2.0)))
-
-
-# d1 = 9 : 1.27
-# d1 = 10: 0.96
-
-vfun = np.vectorize(f)
+        return (9 - ff(p)) + p*(14 - ff(p))
 
 
 def plot(func):
@@ -27,9 +25,10 @@ def plot(func):
     plt.rcParams["figure.figsize"] = [7.50, 3.50]
     plt.rcParams["figure.autolayout"] = True
 
+    vfun = np.vectorize(func)
     p = np.linspace(0, 4, 100)
 
-    plt.plot(p, func(p), color='red')
+    plt.plot(p, vfun(p), color='red')
     plt.plot([res.x], [res.fun], marker="o", markersize=5, 
         markeredgecolor="black", markerfacecolor="black", 
         label=("Min cost: p=" + str(float("{:.4f}".format(res.x))) + ", W=" + str(float("{:.4f}".format(res.fun)))) )
@@ -40,4 +39,4 @@ def plot(func):
     
 if __name__ == '__main__':
 
-    plot(vfun)
+    plot(f)
